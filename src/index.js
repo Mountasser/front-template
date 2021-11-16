4df3779232575829fb261cbafb3f4b3a24fadaac
MOUNTASSER KHAL <k.mountssir@attijariwafa.com>
2021-11-17 00:46:35 +0100

    all files commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Overview from './pages/Overview';
+import NoMatch from './layouts/Sidebar/NotFound';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+      <Route path='/'  element={<App/>} >
+        <Route path='/overview' exact element={<Overview/>} />
+      </Route>
+      <Route path='*' exact element={<NoMatch/>} />
+      </Routes>
+    </Router>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
